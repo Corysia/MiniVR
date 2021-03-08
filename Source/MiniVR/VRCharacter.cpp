449ecbe010ba459252a0188921966ac7cd5f5001
Corysia Taware <corysia.taware@gmail.com>
2021-03-08 13:10:18 -0800

    initial checkin

---
+++ Source/MiniVR/VRCharacter.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRCharacter.h"
+#include <Components/InputComponent.h>
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+    PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+}
+
+void AVRCharacter::MoveForward(const float Throttle)
+{
+    AddMovementInput(Throttle * Camera->GetForwardVector());
+}
+
+void AVRCharacter::MoveRight(const float Throttle)
+{
+    AddMovementInput(Throttle * Camera->GetRightVector());
+}
+
+
